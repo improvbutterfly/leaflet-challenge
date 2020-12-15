17cbec01c20727fd22e2f678c4fd83f9f46bdf78
improvbutterfly <68628212+improvbutterfly@users.noreply.github.com>
2020-12-15 15:16:04 -0800

    Displaying earthquakes as markers on map

---
+++ Leaflet-Step-1/static/js/logic.js
@@ -0,0 +1,25 @@
+// Creating map object
+var myMap = L.map("map", {
+  center: [37.7749, -122.4194],
+  zoom: 5
+});
+
+// Adding tile layer
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+}).addTo(myMap);
+
+// Store our API endpoint inside queryUrl
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+// Perform a GET request to the query URL
+d3.json(queryUrl, function(data) {
+  // Once we get a response, send the data.features object to the createFeatures function
+  //console.log(data.features);
+  L.geoJson(data.features).addTo(myMap);
+});
